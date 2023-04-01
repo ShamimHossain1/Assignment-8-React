6804b6c9e1a4e9a8cf76f628bbb0351b0481b1fd
ShamimHossain1 <beastb697@gmail.com>
2023-04-01 22:11:02 +0600

    answer all questions

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -10,15 +10,15 @@ const Questions = () => {
             </div>
             <div className='question'>
                 <h3>2. How does useState work? </h3>
-                <p>Ans:</p>
+                <p>Ans: useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it. The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.</p>
             </div>
             <div className='question'>
                 <h3>3. Purpose of useEffect other than fetching data. </h3>
-                <p>Ans:</p>
+                <p>Ans: The useEffect in react js allows you to perform side effects in your components. The react useEffect examples of side effects include retrieving data, direct DOM updates, and timers.</p>
             </div>
             <div className='question'>
                 <h3>4. How Does React work?</h3>
-                <p>Ans:</p>
+                <p>Ans: ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props.</p>
             </div>
 
         </div>
