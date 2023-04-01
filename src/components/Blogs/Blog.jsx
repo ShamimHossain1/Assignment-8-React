10d5ae4c1e87ca0cd362b852871c0329effc7094
ShamimHossain1 <beastb697@gmail.com>
2023-04-01 22:55:02 +0600

    toast

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Blog.css'
 
 const Blog = (props) => {
     const { author_name, blog_title, cover_img, hash_tag, pro_images, publish_date, read_time } = props.blog;
     const readingTime = props.readingTime;
     const bookMarked = props.bookMarked;
+
+   const notify=()=>{
+           toast("Bookmarked");
+          
+   }
+
+    const handleClick = () => {
+        notify();
+        bookMarked(blog_title)
+      };
    
     return (
         <div className='blog-body'>
@@ -20,15 +32,18 @@ const Blog = (props) => {
                     </div>
 
                 </div>
+                 <div className="bookmark">
 
-                <p>{read_time} min read <FontAwesomeIcon className='bookmark-icon' onClick={() => bookMarked(blog_title)} icon={faBookmark} /> </p>
+                {read_time} min to read <div ><FontAwesomeIcon className='bookmark-icon' onClick={handleClick}  icon={faBookmark} />
+                </div> 
+                 </div>
             </div>
             <h2>{blog_title}</h2>
             <p>{hash_tag}</p>
             <button className='read-btn' onClick={() => readingTime(read_time)}>Mark as read</button>
             <hr />
 
-
+            <ToastContainer />
         </div>
     );
 };
